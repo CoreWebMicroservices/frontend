e4db2d1881a9ffaf03018da191316288e44facd5
Dima <dmi.mishchenko@gmail.com>
2025-12-17 21:26:57 +0100

    Fix in lint

--- src/translation/config.ts
+++ src/translation/config.ts
@@ -1,4 +1,4 @@
-// @ts-expect-error
+// @ts-expect-error _env_ is injected at runtime for environment config
 const env = window._env_ || {};
 
 const CORE_MS_MS_BASE_URL = env.REACT_COREMS_BASE_URL || import.meta.env.REACT_COREMS_BASE_URL;
